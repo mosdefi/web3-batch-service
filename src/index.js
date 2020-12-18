3c1d5919532a316edb224a4f7817fd5ea01695d1
mosdefi <mosdefi@protonmail.com>
2020-12-18 16:38:26 +0100

    Bumping version to 1.0.1 which supports blockHeight and blockResolution from web3-batch-call 1.0.9

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import BatchCall from "web3-batch-call";
 import regeneratorRuntime from "regenerator-runtime";
 require('dotenv').config()
+const { URL } = require('url');
+const { parse: parseQuery } = require('querystring');
 
 const provider = process.env.PROVIDER_URL;
 const etherscanApiKey = process.env.ETHERSCAN_API_KEY;
@@ -10,6 +12,7 @@ const express = require('express');
 const app = express();
 const batchCall = new BatchCall({
   groupByNamespace: true,
+  simplifyResponse: true,
   provider,
   etherscan: {
     apiKey: etherscanApiKey,
@@ -17,16 +20,31 @@ const batchCall = new BatchCall({
   },
 });
 
+const serverOrigin = `http://localhost:${port}`;
+
 app.use(express.json());
 
 app.listen(port, () => {
-  console.log(`web3-batch-server listening at http://localhost:${port}`);
+  console.log(`web3-batch-service listening at ${serverOrigin}`);
 });
 
 app.post('/', async (req, res) => {
-  res.send(await web3BatchCall(req.body));
+  const url = new URL(req.url, serverOrigin);
+  const callOptions = getBlockHeightAndResolution(url);
+  const response = await web3BatchCall(req.body, callOptions);
+
+  res.send(response);
 });
 
-async function web3BatchCall(contracts) {
-  return await batchCall.execute(contracts);
+async function web3BatchCall(contracts, callOptions) {
+  return await batchCall.execute(contracts, callOptions);
+}
+
+function getBlockHeightAndResolution(url) {
+  const parsedUrl = parseQuery(url.search.substr(1));
+  let { blockHeight, blockResolution } = parsedUrl;
+  blockHeight = parseInt(blockHeight) || 1;
+  blockResolution = parseInt(blockResolution) || 1;
+
+  return { blockHeight, blockResolution };
 }
